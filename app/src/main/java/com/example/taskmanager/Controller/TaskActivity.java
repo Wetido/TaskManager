e72a62923371cfdce75c2510d408a862dcd6c999
Jakub <wetido@gmail.com>
2020-09-18 13:35:15 +0200

    Added mapping elements on recycler view and on click event.

--- app/src/main/java/com/example/taskmanager/Controller/TaskActivity.java
+++ app/src/main/java/com/example/taskmanager/Controller/TaskActivity.java
@@ -6,8 +6,7 @@ import com.example.taskmanager.Fragments.SingleFragmentActivity;
 import com.example.taskmanager.Fragments.TaskFragment;
 
 public class TaskActivity extends SingleFragmentActivity {
-
-
+    
     @Override
     protected Fragment createFragment() {
         return new TaskFragment();
--- app/src/main/java/com/example/taskmanager/Fragments/TaskListFragment.java
+++ app/src/main/java/com/example/taskmanager/Fragments/TaskListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.taskmanager.Model.Task;
 import com.example.taskmanager.Model.TaskLab;
 import com.example.taskmanager.R;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -46,8 +50,30 @@ public class TaskListFragment extends Fragment {
     }
 
     private class TaskHolder extends RecyclerView.ViewHolder{
+        private Task mTask;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
         public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_task,parent, false));
+
+            mTitleTextView = itemView.findViewById(R.id.task_title);
+            mDateTextView = itemView.findViewById(R.id.task_date);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View View) {
+
+                    Toast.makeText(getActivity(), mTask.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        public void bind(Task task){
+
+            mTask = task;
+            mTitleTextView.setText(mTask.getTitle());
+            mDateTextView.setText(mTask.getDate().toString());
         }
     }
 
@@ -66,7 +92,8 @@ public class TaskListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
-
+            Task task = tasks.get(position);
+            holder.bind(task);
         }
 
         @Override
